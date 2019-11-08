1da2a61a3f78ce53e7c7a7791519155f401ade93
Anand Gupta <agupta04@outlook.com>
2019-11-07 20:18:01 -0800

    Read lines from MSFT.csv and print to console

--- StockAnalysis/src/stockAnalysis.java
+++ StockAnalysis/src/stockAnalysis.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class stockAnalysis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello World!");
+		File file = new File("C:\\Users\\Anurag\\StockAnalysis\\MSFT.csv");
+		
+		Scanner sc;
 
+		try {
+			sc = new Scanner(file);
+			
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("File not found.");
+		}
 	}
 
 }
