9f69de058d1e5c6fa7a94ab566f26a7d10498ab4
Anand Gupta <agupta04@outlook.com>
2019-11-11 20:57:04 -0800

    Find the average of closing price

--- StockAnalysis/src/stockAnalysis.java
+++ StockAnalysis/src/stockAnalysis.java
@@ -1,4 +1,5 @@
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class stockAnalysis {
@@ -11,14 +12,29 @@ public class stockAnalysis {
 		try {
 			sc = new Scanner(file);
 			
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+//			if (sc.hasNextLine())
+//			{
+				sc.nextLine();
+//			}
+			double sum = 0;
+			int count=0;
+			while(sc.hasNextLine()) {	
+				var line = sc.nextLine();
+				var data = line.split(",");
+				var doubleData = Double.valueOf(data[4]); 
+				sum+=doubleData;
+				count++;
+				//System.out.printf("%f, %f",doubleData,sum);
+				//System.out.println();
 			}
+			System.out.printf("Average = %f", sum/count);
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.err.println("File not found.");
 		}
+		
 	}
 
 }
