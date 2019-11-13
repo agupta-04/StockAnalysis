f4bce25d036ac74471636d4826223d97bbcad2c1
Anand Gupta <agupta04@outlook.com>
2019-11-12 20:32:44 -0800

    Add new class StockDailyDetails and store the datra in the list of StockDailyDetails

---
+++ StockAnalysis/src/StockDailyDetails.java
@@ -0,0 +1,9 @@
+public class StockDailyDetails {
+	// System.DateTime Date;
+	double OpenPrice;
+	double HighPrice;
+	double LowPrice;
+	double ClosePrice;
+	double AdjClosePrice;
+	long Volume;
+}
--- StockAnalysis/src/stockAnalysis.java
+++ StockAnalysis/src/stockAnalysis.java
@@ -2,32 +2,53 @@ import java.io.File;
 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import java.util.*;
+
 public class stockAnalysis {
 
 	public static void main(String[] args) {
 		File file = new File("C:\\Users\\Anurag\\StockAnalysis\\MSFT.csv");
 		
-		Scanner sc;
-
+		List<StockDailyDetails> stockDailyDetailList = new ArrayList<StockDailyDetails>();
+		
 		try {
-			sc = new Scanner(file);
-			
-//			if (sc.hasNextLine())
-//			{
-				sc.nextLine();
-//			}
-			double sum = 0;
-			int count=0;
+			Scanner sc = new Scanner(file);
+
+			// skip header
+			sc.nextLine();
+
 			while(sc.hasNextLine()) {	
 				var line = sc.nextLine();
 				var data = line.split(",");
-				var doubleData = Double.valueOf(data[4]); 
-				sum+=doubleData;
-				count++;
-				//System.out.printf("%f, %f",doubleData,sum);
-				//System.out.println();
+				var openPrice = Double.valueOf(data[1]);
+				var highPrice = Double.valueOf(data[2]);
+				var lowPrice = Double.valueOf(data[3]);
+				var closePrice = Double.valueOf(data[4]); 
+				var adjClosePrice = Double.valueOf(data[5]);
+				var volume = Long.valueOf(data[6]);
+				
+				var stockDetail = new StockDailyDetails();
+				stockDetail.OpenPrice = openPrice;
+				stockDetail.HighPrice = highPrice;
+				stockDetail.LowPrice = lowPrice;
+				stockDetail.ClosePrice = closePrice;
+				stockDetail.AdjClosePrice = adjClosePrice;
+				stockDetail.Volume = volume;
+				
+				stockDailyDetailList.add(stockDetail);
 			}
-			System.out.printf("Average = %f", sum/count);
+			
+			var count = stockDailyDetailList.size();
+			double openSum = 0.0;
+			double closeSum = 0.0;
+			for (StockDailyDetails stockDailyDetails : stockDailyDetailList) {
+				openSum += stockDailyDetails.OpenPrice;
+				closeSum += stockDailyDetails.ClosePrice;
+			}
+			
+			System.out.printf("Average Open Price = %f\r\n", openSum/count);
+			System.out.printf("Average Close Price = %f\r\n", closeSum/count);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
